5902574ede5cc6ee63866eb28415d5ed18f14772
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2026-05-06 18:16:17 +0200

    feat(mqtt): add declarative sensors

--- examples/mqtt/main.py
+++ examples/mqtt/main.py
@@ -2,51 +2,112 @@ import os
 import time
 import json
 import dotenv
+from dataclasses import dataclass
 from foxess import FoxESSClient, FoxESSInverter
 from paho.mqtt.client import CallbackAPIVersion as MQTTCallbackAPIVersion, Client as MQTTClient
 
 dotenv.load_dotenv()
 
-foxess = FoxESSClient(os.getenv('FOXESS_TOKEN'))
-inverter = FoxESSInverter(foxess, os.getenv('FOXESS_INVERTER'))
+FOXESS_TOKEN = os.getenv('FOXESS_TOKEN')
+FOXESS_INVERTER = os.getenv('FOXESS_INVERTER')
+MQTT_HOST = os.getenv('MQTT_HOST')
+MQTT_PORT = os.getenv('MQTT_PORT', 1883)
+
+foxess = FoxESSClient(FOXESS_TOKEN)
+inverter = FoxESSInverter(foxess, FOXESS_INVERTER)
 mqtt = MQTTClient(MQTTCallbackAPIVersion.VERSION2)
 
+sensors = [
+    {
+        'name': 'power',
+        'label': 'Power',
+        'class': 'power',
+        'unit': 'kW',
+        'get': lambda: inverter.get_power(),
+    },
+    {
+        'name': 'voltage_l1',
+        'label': 'Voltage L1',
+        'class': 'voltage',
+        'unit': 'V',
+        'get': lambda: inverter.get_grid_voltages()[0],
+    },
+    {
+        'name': 'voltage_l2',
+        'label': 'Voltage L2',
+        'class': 'voltage',
+        'unit': 'V',
+        'get': lambda: inverter.get_grid_voltages()[1],
+    },
+    {
+        'name': 'voltage_l3',
+        'label': 'Voltage L3',
+        'class': 'voltage',
+        'unit': 'V',
+        'get': lambda: inverter.get_grid_voltages()[2],
+    },
+    {
+        'name': 'frequency_l1',
+        'label': 'Frequency L1',
+        'class': 'frequency',
+        'unit': 'Hz',
+        'get': lambda: inverter.get_grid_frequencies()[0],
+    },
+    {
+        'name': 'frequency_l2',
+        'label': 'Frequency L2',
+        'class': 'frequency',
+        'unit': 'Hz',
+        'get': lambda: inverter.get_grid_frequencies()[1],
+    },
+    {
+        'name': 'frequency_l3',
+        'label': 'Frequency L3',
+        'class': 'frequency',
+        'unit': 'Hz',
+        'get': lambda: inverter.get_grid_frequencies()[2],
+    },
+]
+
 def main():
-    mqtt.connect('mqtt.sourcloud.pl', 1883, 60)
+    mqtt.connect(MQTT_HOST, MQTT_PORT, 60)
     mqtt.on_connect = on_connect
 
     mqtt.loop_start()
 
     while True:
-        power = inverter.get_power()
-        mqtt.publish('foxess/power', power)
+        for sensor in sensors:
+            mqtt.publish(f'foxess/{inverter.sn}/{sensor['name']}', sensor['get']())
 
-        time.sleep(10)
+        time.sleep(60)
 
     mqtt.loop_stop()
 
 def on_connect(client, *args):
+    components = {}
+    for sensor in sensors:
+        components[sensor['name']] = {
+            'unique_id': f'{inverter.sn}_{sensor['name']}',
+            'name': sensor['label'],
+            'platform': 'sensor',
+            'device_class': sensor['class'],
+            'unit_of_measurement': sensor['unit'],
+            'state_topic': f'foxess/{inverter.sn}/{sensor['name']}',
+        }
+
     client.publish(f'homeassistant/device/{inverter.sn}/config', json.dumps({
         'device': {
             'identifiers': [inverter.sn],
             'name': f'FoxESS {inverter.model}',
             'manufacturer': 'FoxESS',
             'model': inverter.model,
-            'sn': inverter.sn,
+            'serial_number': inverter.sn,
+            'sw_version': inverter.version,
         },
         'origin': {
             'name': 'python_foxess',
         },
-        'components': {
-            'foxess_power': {
-                'unique_id': 'foxess_power',
-                'name': 'Power',
-                'platform': 'sensor',
-                'device_class': 'power',
-                'unit_of_measurement': 'kW',
-                'state_topic': 'foxess/power',
-            },
-        },
+        'components': components,
     }))
 
 if __name__ == '__main__':
