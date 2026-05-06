e392197a1d496216b62800cd5df2e40f0b13bd23
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2026-05-06 18:12:32 +0200

    feat: add grid voltages and frequencies

--- foxess/inverter.py
+++ foxess/inverter.py
@@ -57,23 +57,57 @@ class FoxESSInverter:
 
     @property
     def model(self) -> str:
+        """Model name.
+        """
+
         assert 'deviceType' in self._details and isinstance(self._details['deviceType'], str)
         return self._details['deviceType']
 
     @property
     def version(self) -> str:
+        """Software version.
+        """
+
         assert 'masterVersion' in self._details and isinstance(self._details['masterVersion'], str)
         return self._details['masterVersion']
 
     def get_status(self) -> FoxESSInverterStatus:
+        """Return current status of inverter.
+        """
+
         self._refresh_details()
         assert 'status' in self._details and isinstance(self._details['status'], int)
 
         return FoxESSInverterStatus.from_code(self._details['status'])
 
     def get_power(self) -> float:
+        """Return value of currently produced power in kWh.
+        """
+
         self._refresh_variables()
         assert 'pvPower' in self._variables and isinstance(self._variables['pvPower'], float)
 
         return self._variables['pvPower']
+
+    def get_grid_voltages(self) -> tuple[float, float, float]:
+        """Return values of grid voltages in V.
+        """
+
+        self._refresh_variables()
+        assert 'RVolt' in self._variables and isinstance(self._variables['RVolt'], float)
+        assert 'SVolt' in self._variables and isinstance(self._variables['SVolt'], float)
+        assert 'TVolt' in self._variables and isinstance(self._variables['TVolt'], float)
+
+        return (self._variables['RVolt'], self._variables['SVolt'], self._variables['TVolt'])
+
+    def get_grid_frequencies(self) -> tuple[float, float, float]:
+        """Return frequencies of grid voltages in Hz.
+        """
+
+        self._refresh_variables()
+        assert 'RFreq' in self._variables and isinstance(self._variables['RFreq'], float)
+        assert 'SFreq' in self._variables and isinstance(self._variables['SFreq'], float)
+        assert 'TFreq' in self._variables and isinstance(self._variables['TFreq'], float)
+
+        return (self._variables['RFreq'], self._variables['SFreq'], self._variables['TFreq'])
 
